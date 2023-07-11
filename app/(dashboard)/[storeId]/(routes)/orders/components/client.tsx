cbc9759b36ad0af7f765efdcc6ba7eee57c3d36f
Rahdeg <walett95@gmail.com>
2023-07-11 15:35:34 +0100

    ss

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -2,18 +2,16 @@
 
 import Heading from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
-import { useParams, useRouter } from "next/navigation"
 import { OrderColumn, columns } from "./column"
 import { DataTable } from "@/components/ui/data-table"
-import { ApiList } from "@/components/ui/api-list"
+
 
 interface OrderClientProps{
     data : OrderColumn[]
 }
 
 export const OrderClient : React.FC<OrderClientProps> =({data})=>{
-    const router = useRouter();
-    const params = useParams();
+    
     return (
         <>
         <div className=" flex items-center justify-between">
